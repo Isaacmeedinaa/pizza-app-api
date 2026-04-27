48c0bc6a04230e6935ebbccaaf8de093ef7ff2c7
Isaac Medina <isaac.meedinaa@gmail.com>
2026-04-27 01:21:04 -0500

    updated response data

--- src/routes/pizzaRouter.ts
+++ src/routes/pizzaRouter.ts
@@ -4,7 +4,7 @@ import pizzas from "../data/pizzas.json" with { type: "json" };
 const pizzaRouter = Router();
 
 pizzaRouter.get("/", (_req, res) => {
-  res.json(pizzas);
+  res.status(200).send({ data: { pizzas: pizzas } });
 });
 
 export default pizzaRouter;
